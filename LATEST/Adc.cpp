3c3b9ed7a13dabe17bcef20eed64d8cb58ba0050
root <nagaraja.hm.0001@gmail.com>
2021-12-24 22:58:07 +0100

    Update: submodule Adc

---
+++ LATEST/Adc.cpp
@@ -0,0 +1,10 @@
+#include "Adc.h"
+
+class_Adc Adc;
+
+void class_Adc::InitFunction(void){
+}
+
+void class_Adc::MainFunction(void){
+}
+
---
+++ LATEST/Adc.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class class_Adc{
+   public:
+      void InitFunction(void);
+      void MainFunction(void);
+};
+
+extern class_Adc Adc;
+
