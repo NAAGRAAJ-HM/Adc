345bea5e1998d9963fcb70e51d3db600f5399c04
root <nagaraja.hm.0001@gmail.com>
2022-01-12 12:18:12 +0100

    Update: submodule Adc

--- LATEST/Adc.cpp
+++ LATEST/Adc.cpp
@@ -8,3 +8,39 @@ FUNC(void, ADC_CODE) class_Adc::InitFunction(void){
 FUNC(void, ADC_CODE) class_Adc::MainFunction(void){
 }
 
+FUNC(void, ADC_CODE) class_Adc::DeInitFunction(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::SetupResultBuffer(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::StartGroupConversion(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::StopGroupConversion(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::ReadGroup(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::EnableHardwareTrigger(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::DisableHardwareTrigger(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::EnableGroupNotification(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::DisableGroupNotification(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::GetGroupStatus(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::GetStreamLastPointer(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::GetVersionInfo(void){
+}
+
--- LATEST/Adc.h
+++ LATEST/Adc.h
@@ -5,8 +5,20 @@
 
 class class_Adc{
    public:
-      FUNC(void, ADC_CODE) InitFunction(void);
-      FUNC(void, ADC_CODE) MainFunction(void);
+      FUNC(void, ADC_CODE) InitFunction             (void);
+      FUNC(void, ADC_CODE) MainFunction             (void);
+      FUNC(void, ADC_CODE) DeInitFunction           (void);
+      FUNC(void, ADC_CODE) SetupResultBuffer        (void);
+      FUNC(void, ADC_CODE) StartGroupConversion     (void);
+      FUNC(void, ADC_CODE) StopGroupConversion      (void);
+      FUNC(void, ADC_CODE) ReadGroup                (void);
+      FUNC(void, ADC_CODE) EnableHardwareTrigger    (void);
+      FUNC(void, ADC_CODE) DisableHardwareTrigger   (void);
+      FUNC(void, ADC_CODE) EnableGroupNotification  (void);
+      FUNC(void, ADC_CODE) DisableGroupNotification (void);
+      FUNC(void, ADC_CODE) GetGroupStatus           (void);
+      FUNC(void, ADC_CODE) GetStreamLastPointer     (void);
+      FUNC(void, ADC_CODE) GetVersionInfo           (void);
 };
 
 extern class_Adc Adc;
