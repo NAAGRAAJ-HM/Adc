6f2187a45ac6bac7d2cdaa7777f158573b090b8c
root <nagaraja.hm.0001@gmail.com>
2022-02-01 09:35:59 +0100

    Update: submodule Adc

---
+++ LATEST/Adc1.cpp
@@ -0,0 +1,46 @@
+#include "Adc.h"
+
+class_Adc Adc;
+
+FUNC(void, ADC_CODE) class_Adc::InitFunction(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::MainFunction(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::DeInitFunction(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::SetupResultBuffer(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::StartGroupConversion(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::StopGroupConversion(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::ReadGroup(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::EnableHardwareTrigger(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::DisableHardwareTrigger(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::EnableGroupNotification(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::DisableGroupNotification(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::GetGroupStatus(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::GetStreamLastPointer(void){
+}
+
+FUNC(void, ADC_CODE) class_Adc::GetVersionInfo(void){
+}
+
