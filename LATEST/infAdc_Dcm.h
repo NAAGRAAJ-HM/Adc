251e78618c88b75b34c429fd43a572b41a664a95
root <nagaraja.hm.0001@gmail.com>
2022-03-11 12:12:05 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Adc

--- LATEST/infAdc_Dcm.h
+++ LATEST/infAdc_Dcm.h
@@ -1,15 +1,13 @@
+#pragma once
 /*****************************************************/
-/* File   : Adc.cpp                                  */
+/* File   : infAdc_Dcm.h                             */
 /* Author : Naagraaj HM                              */
 /*****************************************************/
 
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
-#include "module.h"
-#include "infAdc_EcuM.h"
-#include "infAdc_Dcm.h"
-#include "infAdc_SchM.h"
+#include "Compiler_Cfg_Adc.h"
 
 /*****************************************************/
 /* #DEFINES                                          */
@@ -22,15 +20,6 @@
 /*****************************************************/
 /* TYPEDEFS                                          */
 /*****************************************************/
-class module_Adc:
-      public abstract_module
-{
-   public:
-      FUNC(void, ADC_CODE) InitFunction   (void);
-      FUNC(void, ADC_CODE) DeInitFunction (void);
-      FUNC(void, ADC_CODE) GetVersionInfo (void);
-      FUNC(void, ADC_CODE) MainFunction   (void);
-};
 
 /*****************************************************/
 /* CONSTS                                            */
@@ -43,57 +32,11 @@ class module_Adc:
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-module_Adc     Adc;
-infEcuMClient* gptrinfEcuMClient_Adc = &Adc;
-infDcmClient*  gptrinfDcmClient_Adc  = &Adc;
-infSchMClient* gptrinfSchMClient_Adc = &Adc;
+extern infDcmClient* gptrinfDcmClient_Adc;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-FUNC(void, ADC_CODE) module_Adc::InitFunction(void){
-}
-
-FUNC(void, ADC_CODE) module_Adc::DeInitFunction(void){
-}
-
-FUNC(void, ADC_CODE) module_Adc::GetVersionInfo(void){
-}
-
-FUNC(void, ADC_CODE) module_Adc::MainFunction(void){
-}
-
-#include "Adc_Unused.h"
-
-FUNC(void, ADC_CODE) class_Adc_Unused::SetupResultBuffer(void){
-}
-
-FUNC(void, ADC_CODE) class_Adc_Unused::StartGroupConversion(void){
-}
-
-FUNC(void, ADC_CODE) class_Adc_Unused::StopGroupConversion(void){
-}
-
-FUNC(void, ADC_CODE) class_Adc_Unused::ReadGroup(void){
-}
-
-FUNC(void, ADC_CODE) class_Adc_Unused::EnableHardwareTrigger(void){
-}
-
-FUNC(void, ADC_CODE) class_Adc_Unused::DisableHardwareTrigger(void){
-}
-
-FUNC(void, ADC_CODE) class_Adc_Unused::EnableGroupNotification(void){
-}
-
-FUNC(void, ADC_CODE) class_Adc_Unused::DisableGroupNotification(void){
-}
-
-FUNC(void, ADC_CODE) class_Adc_Unused::GetGroupStatus(void){
-}
-
-FUNC(void, ADC_CODE) class_Adc_Unused::GetStreamLastPointer(void){
-}
 
 /*****************************************************/
 /* EOF                                               */
