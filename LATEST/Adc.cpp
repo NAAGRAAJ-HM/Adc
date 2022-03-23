42369a23fd9d4b46cf69200854674db9ba5bcbcd
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:07:22 +0100

    Update: AUTOSAR basic software micro-controller abstraction module Adc

--- LATEST/Adc.cpp
+++ LATEST/Adc.cpp
@@ -40,6 +40,16 @@ class module_Adc:
       FUNC(void, ADC_CODE) DeInitFunction (void);
       FUNC(void, ADC_CODE) GetVersionInfo (void);
       FUNC(void, ADC_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, ADC_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
