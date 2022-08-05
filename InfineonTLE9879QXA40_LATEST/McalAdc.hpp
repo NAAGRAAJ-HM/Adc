c4678f9c1365f116f797e46ea17fbed51428e8b2
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-05 14:36:56 +0200

    Update: AUTOSAR basic software micro-controller abstraction driver module for analog to digital converter

---
+++ InfineonTLE9879QXA40_LATEST/McalAdc.hpp
@@ -0,0 +1,72 @@
+#pragma once
+/******************************************************************************/
+/* File   : McalAdc.hpp                                                           */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "ConstMcalAdc.hpp"
+#include "CfgMcalAdc.hpp"
+#include "McalAdc_core.hpp"
+#include "infMcalAdc_Exp.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class module_McalAdc:
+      INTERFACES_EXMCALPORTED_MCALADC
+      public abstract_module
+   ,  public class_McalAdc_Functionality
+{
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+      const ConstMcalAdc_Type* lptrConst = (ConstMcalAdc_Type*)NULL_PTR;
+
+   public:
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+      FUNC(void, MCALADC_CODE) InitFunction(
+            CONSTP2CONST(ConstModule_TypeAbstract, MCALADC_CONST,       MCALADC_APPL_CONST) lptrConstModule
+         ,  CONSTP2CONST(CfgModule_TypeAbstract,   MCALADC_CONFIG_DATA, MCALADC_APPL_CONST) lptrCfgModule
+      );
+      FUNC(void, MCALADC_CODE) DeInitFunction (void);
+      FUNC(void, MCALADC_CODE) MainFunction   (void);
+      MCALADC_CORE_FUNCTIONALITIES
+#if(STD_ON == McalAdc_SupportStatePowerLow)
+      FUNC(void,               MCALADC_CODE) PreparePowerState    (McalAdc_TypeStatePower lstStatePowerTargetRequested);
+      FUNC(void,               MCALADC_CODE) SetPowerState        (void);
+      FUNC(McalAdc_TypeStatePower, MCALADC_CODE) GetCurrentPowerState (void);
+      FUNC(McalAdc_TypeStatePower, MCALADC_CODE) GetTargetPowerState  (void);
+#endif
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+extern VAR(module_McalAdc, MCALADC_VAR) McalAdc;
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
---
+++ InfineonTLE9879QXA40_LATEST/McalAdc_core.hpp
@@ -0,0 +1,70 @@
+#pragma once
+/******************************************************************************/
+/* File   : McalAdc_core.hpp                                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "CompilerCfg_McalAdc.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define MCALADC_CORE_FUNCTIONALITIES                                               \
+              FUNC(void, MCALADC_CODE) SetupResultBuffer        (void);            \
+              FUNC(void, MCALADC_CODE) StartGroupConversion     (McalAdc_TypeChannelGroup* lpstChannelGroup);     \
+              FUNC(void, MCALADC_CODE) StopGroupConversion      (McalAdc_TypeChannelGroup* lpstChannelGroup);     \
+              FUNC(void, MCALADC_CODE) ReadGroup                (McalAdc_TypeChannelGroup* lpstChannelGroup);     \
+              FUNC(void, MCALADC_CODE) EnableHardwareTrigger    (McalAdc_TypeChannelGroup* lpstChannelGroup);     \
+              FUNC(void, MCALADC_CODE) DisableHardwareTrigger   (McalAdc_TypeChannelGroup* lpstChannelGroup);     \
+              FUNC(void, MCALADC_CODE) EnableGroupNotification  (void); /*McalAdc_TypeChannelGroup* lpstChannelGroup);*/ \
+              FUNC(void, MCALADC_CODE) DisableGroupNotification (void); /*McalAdc_TypeChannelGroup* lpstChannelGroup);*/ \
+              FUNC(void, MCALADC_CODE) GetGroupStatus           (void);            \
+              FUNC(void, MCALADC_CODE) GetStreamLastPointer     (McalAdc_TypeChannelGroup* lpstChannelGroup);     \
+
+#define MCALADC_CORE_FUNCTIONALITIES_VIRTUAL                                       \
+      virtual FUNC(void, MCALADC_CODE) SetupResultBuffer        (void) = 0;        \
+      virtual FUNC(void, MCALADC_CODE) StartGroupConversion     (McalAdc_TypeChannelGroup* lpstChannelGroup) = 0; \
+      virtual FUNC(void, MCALADC_CODE) StopGroupConversion      (McalAdc_TypeChannelGroup* lpstChannelGroup) = 0; \
+      virtual FUNC(void, MCALADC_CODE) ReadGroup                (McalAdc_TypeChannelGroup* lpstChannelGroup) = 0; \
+      virtual FUNC(void, MCALADC_CODE) EnableHardwareTrigger    (McalAdc_TypeChannelGroup* lpstChannelGroup) = 0; \
+      virtual FUNC(void, MCALADC_CODE) DisableHardwareTrigger   (McalAdc_TypeChannelGroup* lpstChannelGroup) = 0; \
+      virtual FUNC(void, MCALADC_CODE) EnableGroupNotification  (void) = 0; /*McalAdc_TypeChannelGroup* lpstChannelGroup) = 0;*/ \
+      virtual FUNC(void, MCALADC_CODE) DisableGroupNotification (void) = 0; /*McalAdc_TypeChannelGroup* lpstChannelGroup) = 0;*/ \
+      virtual FUNC(void, MCALADC_CODE) GetGroupStatus           (void) = 0;        \
+      virtual FUNC(void, MCALADC_CODE) GetStreamLastPointer     (McalAdc_TypeChannelGroup* lpstChannelGroup) = 0; \
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class class_McalAdc_Functionality{
+   public:
+      MCALADC_CORE_FUNCTIONALITIES_VIRTUAL
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
