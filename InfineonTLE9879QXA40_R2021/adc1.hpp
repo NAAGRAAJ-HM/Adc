c105be838e58c7c432fc22184dcfa6dfaff66b40
Raajnaag HULIYAPURADA MATA <nagaraja.hm.0001@gmail.com>
2024-06-12 20:01:20 +0530

    Initial commit

--- InfineonTLE9879QXA40_R2021/adc1.hpp
+++ InfineonTLE9879QXA40_R2021/adc1.hpp
@@ -1,24 +1,8 @@
 #pragma once
 /******************************************************************************/
 /* File   : Template.hpp                                                      */
-/*                                                                            */
-/* Author : Nagaraja HULIYAPURADA MATA                                        */
-/*                                                                            */
-/* License / Warranty / Terms and Conditions                                  */
-/*                                                                            */
-/* Everyone is permitted to copy and distribute verbatim copies of this lice- */
-/* nse document, but changing it is not allowed. This is a free, copyright l- */
-/* icense for software and other kinds of works. By contrast, this license is */
-/* intended to guarantee your freedom to share and change all versions of a   */
-/* program, to make sure it remains free software for all its users. You have */
-/* certain responsibilities, if you distribute copies of the software, or if  */
-/* you modify it: responsibilities to respect the freedom of others.          */
-/*                                                                            */
-/* All rights reserved. Copyright © 1982 Nagaraja HULIYAPURADA MATA           */
-/*                                                                            */
-/* Always refer latest software version from:                                 */
-/* https://github.com/NagarajaHuliyapuradaMata?tab=repositories               */
-/*                                                                            */
+/* Author : Nagaraja HULIYAPURADA-MATA                                        */
+/* Date   : 01.02.1982                                                        */
 /******************************************************************************/
 
 /******************************************************************************/
@@ -270,7 +254,6 @@ extern TADC1_ANON ADC1_ANON_Sts                       (void);
 extern bool       ADC1_GetVF1                         (void);
 extern bool       ADC1_GetOUT_CH1                     (void);
 extern uint32     ADC1_GetRES_OUT6                    (void);
-
 
 /******************************************************************************/
 /* EOF                                                                        */
